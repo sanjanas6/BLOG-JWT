a4cfbff94dba2433fb5a47b8418f499171c0db98
Sanjana Singh <sanjanasingh7774@gmail.com>
2024-05-08 12:56:19 +0530

    routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Sigin from './pages/Sigin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 export default function App() {
   return (
-    <div>
-     <h1 className='text-3xl text-blue-600'>App</h1> 
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/signin' element={<Sigin />} />
+    <Route path='/signup' element={<Signup />} />
+    <Route path='/projects' element={<Projects />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
